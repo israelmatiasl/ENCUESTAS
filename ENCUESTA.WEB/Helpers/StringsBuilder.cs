6de8bfaa6d263af68b55fa155b38c49335121f18
Israel <israelmatiasl@gmail.com>
2017-10-24 19:28:46 -0500

    PRIMER CAMBIO

---
+++ ENCUESTA.WEB/Helpers/StringsBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ENCUESTA.WEB.Helpers
+{
+    public class StringsBuilder
+    {
+        public static String pregunta1 = "¿Sabe usted si su empresa cuenta con una página web para mostrar información de sus productos?";
+        public static String pregunta2 = "¿Sabe usted si su empresa vende sus productos a través de su página web?";
+        public static String pregunta3 = "¿Sabe usted si su empresa tiene implementado un botón de pago en su página web?";
+        public static String pregunta4 = "¿Sabe usted si los clientes de su empresa poseen dispositivos móviles (featured phones o smartphones)?";
+        public static String pregunta5 = "¿Sabe usted si los clientes de su empresa envían constantemente SMS?";
+        public static String pregunta6 = "¿Sabe usted si su empresa tiene implementado un mecanismo de pago basado en SMS Billing?";
+        public static String pregunta7 = "¿Sabe usted si los clientes de su empresa hacen uso constante de smartphones para generar compras online?";
+        public static String pregunta8 = "¿Sabe usted si su empresa tiene implementado alguna aplicación que haga uso de tecnología QR para la venta de sus productos?";
+        public static String pregunta9 = "¿Sabe usted si los trabajadores de su empresa cuentan con dispositivos smartphone que tengan integrado NFC?";
+        public static String pregunta10 = "¿Sabe usted si su empresa tiene implementado soluciones con tecnología contactless para procesar pagos con NFC?";
+        public static String pregunta11 = "¿Sabe usted si su empresa tiene implementado un dispositivo mobile POS para procesar pagos?";
+        public static String pregunta12 = "¿Sabe usted si su empresa desea automatizar la atención de llamadas de los pedidos de venta de sus clientes?";
+        public static String pregunta13 = "¿Sabe usted si su empresa tiene implementado un mecanismo de pago basado en IVR Billing para la automatización de llamadas de pedidos de venta?";
+        public static String pregunta14 = "¿Sabe usted si su empresa tiene implementado algún servicio de cobro de sus productos mediante el recargo a la factura de una operadora telefónica?";
+        public static String pregunta15 = "¿Sabe usted si su empresa tiene implementado un mecanismo de pago basado en Carrier Billing?";
+        public static String pregunta16 = "¿Sabe usted si su empresa cuenta con una página en Facebook?";
+        public static String pregunta17 = "¿Sabe usted si su empresa vende sus productos a través de su página de facebook?";
+        public static String pregunta18 = "¿Sabe usted si su empresa tiene implementado chatbots para la venta de sus productos o servicios en su página de facebook?";
+        public static String pregunta19 = "¿Sabe usted si su empresa utiliza la aplicación Whatsapp como vía de comunicación con sus clientes?";
+        public static String pregunta20 = "¿Sabe usted si su empresa vende sus productos a través de Whatsapp?";
+        public static String pregunta21 = "¿Sabe usted si su empresa tiene implementada tecnología para transferencia de dinero en Whatsapp?";
+        public static String pregunta22 = "¿Sabe usted si su empresa genera campañas para promocionar sus productos?";
+        public static String pregunta23 = "¿Sabe usted si las campañas de promoción de productos que realiza su empresa son realizadas en medios como revistas o páginas webs?";
+        public static String pregunta24 = "¿Sabe usted si su empresa realiza campañas de descuentos en los productos de sus locales?";
+        public static String pregunta25 = "¿Sabe usted si su empresa tiene implementado Beacons en los locales de su empresa?";
+        public static String pregunta26 = "¿Sabe usted si su empresa genera campañas de fidelización para sus clientes?";
+        public static String pregunta27 = "¿Sabe usted si su empresa emplea Tarjetas de fidelización para potenciar sus ventas?";
+        public static String pregunta28 = "¿Sabe usted si su empresa emplea Tarjetas de descuentos para sus clientes?";
+        public static String pregunta29 = "¿Sabe usted si su empresa trabaja en conjunto con otras empresas para mejorar sus ventas?";
+        public static String pregunta30 = "¿Sabe usted si su empresa emplea Giftcards para impulsar sus ventas?";
+        public static String pregunta31 = "¿Sabe usted si su empresa desearía automatizar los pedidos de productos realizados a su proveedor?";
+        public static String pregunta32 = "¿Sabe usted si su empresa tiene implementado un Dash Button?";
+
+
+        public static String bodyQuest(String pregunta)
+        {
+            String strbody = String.Empty;
+            
+
+            return strbody;
+        }
+    }
+}
